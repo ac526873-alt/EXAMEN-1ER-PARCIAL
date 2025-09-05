732d692692fa1fe008e714c6f7da1704b2c3aabc
JESUS EMMANUEL ACOSTA TORRES <ac526873@uaeh.edu.mx>
2025-09-05 16:37:14 -0600

    Add files via upload

---
+++ multiplicacionDeDosMatrices.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lenguajesdelaprogramacion;
+
+/**
+ *
+ * @author jeatj
+ */
+import java.util.Scanner;
+
+public class multiplicacionDeDosMatrices {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] a = new int[2][2], b = new int[2][2], r = new int[2][2];
+        System.out.println("////presiona Enter al ingresar cada numero////");
+        System.out.println(" Matriz A, Ingresa 4 numeros: ");
+        for (int x = 0; x < 2; x++) {
+            for (int z = 0; z < 2; z++) {
+                a[x][z] = sc.nextInt();
+            }
+        }
+
+        System.out.println(" Matriz B, Ingresa 4 numeros: ");
+        for (int x = 0; x < 2; x++) {
+            for (int z = 0; z < 2; z++) {
+                b[x][z] = sc.nextInt();
+            }
+        }
+
+        for (int x = 0; x < 2; x++) {
+            for (int z = 0; z < 2; z++) {
+                for (int y = 0; y < 2; y++) {
+                    r[x][z] += a[x][y] * b[y][z];
+                }
+            }
+        }
+
+        System.out.println("Resultado:");
+        System.out.println(r[0][0] + " " + r[0][1]);
+        System.out.println(r[1][0] + " " + r[1][1]);
+    }
+}
